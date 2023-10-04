1be3fe01a209a47838d1f1e9f422ee4e1ddeebac
Owoh Chidubem <chidubempaul91@gmail.com>
2023-10-04 12:56:12 +0100

    First push

---
+++ build/ckeditor.d.ts
@@ -0,0 +1,20 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import { ClassicEditor as ClassicEditorBase } from "@ckeditor/ckeditor5-editor-classic";
+export default class ClassicEditor extends ClassicEditorBase {
+    static builtinPlugins: any[];
+    static defaultConfig: {
+        toolbar: {
+            items: string[];
+        };
+        language: string;
+        image: {
+            toolbar: string[];
+        };
+        table: {
+            contentToolbar: string[];
+        };
+    };
+}
